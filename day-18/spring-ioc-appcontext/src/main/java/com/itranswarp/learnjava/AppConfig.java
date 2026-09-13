630dbb544b1d6287ca5404257f3041250307a5a3
wangzheng <everywangbug@gmail.com>
2026-09-13 11:10:30 +0800

    add spring-ioc-appcontext resource

--- day-18/spring-ioc-appcontext/src/main/java/com/itranswarp/learnjava/AppConfig.java
+++ day-18/spring-ioc-appcontext/src/main/java/com/itranswarp/learnjava/AppConfig.java
@@ -1,35 +1,34 @@
 package com.itranswarp.learnjava;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan
-@PropertySource("app.properties")
 public class AppConfig {
 
-    @Value("${jdbc.url}")
-    private String url;
-
-    @Value("${jdbc.username}")
-    private String username;
-
-    @Value("${jdbc.password}")
-    private String password;
+    // 把 app.properties 注入为 Resource，再从 Resource 中读取配置
+    @Value("classpath:app.properties")
+    private Resource appProperties;
 
     @Bean(destroyMethod = "close")
-    public DataSource dataSource() {
+    public DataSource dataSource() throws IOException {
+        Properties props = PropertiesLoaderUtils.loadProperties(appProperties);
         HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(url);
-        ds.setUsername(username);
-        ds.setPassword(password);
+        ds.setJdbcUrl(props.getProperty("jdbc.url"));
+        ds.setUsername(props.getProperty("jdbc.username"));
+        ds.setPassword(props.getProperty("jdbc.password"));
         return ds;
     }
 }
